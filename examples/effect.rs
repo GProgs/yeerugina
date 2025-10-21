6e9abc18c8e90ed345097ebec8ce187da4d3c7a5
Gentrit Zenuni <gentrit.zenuni@gmail.com>
2025-10-22 01:55:56 +0300

    use new_sudden and new_smooth constructors

--- examples/effect.rs
+++ examples/effect.rs
@@ -2,12 +2,14 @@ use yeerugina::cmd::Effect;
 use std::time::Duration;
 
 fn main() {
-	let eff = Effect::new(None);
+	let eff = Effect::new_sudden();
 	println!("Effect 1 is {:?}", eff);
-	let eff2 = Effect::new(Some(Duration::from_millis(555)));
+	let eff2 = Effect::new_smooth(Duration::from_millis(555));
 	println!("Effect 2 should be 555 ms, got {eff2:?}");
 	let eff_def = Effect::default();
 	println!("The default effect is {eff_def:?}");
-	let bad_eff = Effect::new(Some(Duration::default()));
+	let bad_eff = Effect::new_smooth(Duration::default());
 	println!("This bad effect should be 0ms, got {bad_eff:?}");
+        let worse_eff = Effect::new_smooth(Duration::from_millis(10));
+        println!("This should be only 10ms, got {worse_eff:?}");
 }
