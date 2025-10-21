9500e837aa11aa5227b6ee162c0aad1fa2c556f9
Gentrit Zenuni <gentrit.zenuni@gmail.com>
2025-10-21 23:45:23 +0300

    new example

---
+++ examples/effect.rs
@@ -0,0 +1,13 @@
+use crate::cmd::Effect;
+use std::time::Duration;
+
+fn main() {
+	let eff = Effect::new(None);
+	println!("Effect 1 is {:?}", eff);
+	let eff2 = Effect::new(Duration::from_millis(555));
+	println!("Effect 2 should be 555 ms, got {eff2:?}");
+	let eff_def = Effect::default();
+	println!("The default effect is {eff_def:?}");
+	let bad_eff = Effect::new(Duration::default());
+	println!("This bad effect should be 0ms, got {bad_eff:?}");
+}
