8b7cf0813f7a0bb8bb4573be1cfa6f24d78ce9f7
Gentrit Zenuni <gentrit.zenuni@gmail.com>
2025-10-22 18:36:37 +0300

    add associated()

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,4 +1,4 @@
-use log::info;
+use log::{error, info};
 use std::fmt;
 use std::time::Duration;
 use strum_macros;
@@ -27,10 +27,10 @@ pub struct Effect(EffectInner);
 /// Effect::new_sudden().
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
 enum EffectInner {
-        /// A transition that takes place immediately (i.e. without delay).
+	/// A transition that takes place immediately (i.e. without delay).
 	#[default]
 	Sudden,
-        /// A transition that takes place over some time (i.e. smooth fade from red to blue).
+	/// A transition that takes place over some time (i.e. smooth fade from red to blue).
 	Smooth(Duration),
 }
 
@@ -39,13 +39,13 @@ impl Effect {
 	///
 	/// If dur is zero value, the Effect returned will be Sudden.
 	/// Otherwise, dur is used directly. However, if its value is less than 30 milliseconds,
-        /// the value will be clamped to 30 milliseconds.
+	/// the value will be clamped to 30 milliseconds.
 	pub fn new_smooth(dur: Duration) -> Self {
 		match dur {
 			_ if dur.is_zero() => {
-                            info!("Zero duration converted to sudden effect");
-                            Effect(EffectInner::Sudden)
-                        },
+				info!("Zero duration converted to sudden effect");
+				Effect(EffectInner::Sudden)
+			},
 			_ if dur.as_millis() < 30 => {
 				info!("Clamped smooth effect duration to 30 ms");
 				Effect(EffectInner::Smooth(Duration::from_millis(30)))
@@ -54,10 +54,12 @@ impl Effect {
 		}
 	}
 
-        /// Create a Sudden Effect.
-        ///
-        /// This function is quite trivial. It just creates a struct containing the Sudden value.
-        pub fn new_sudden() -> Self { Effect(EffectInner::Sudden) }
+	/// Create a Sudden Effect.
+	///
+	/// This function is quite trivial. It just creates a struct containing the Sudden value.
+	pub fn new_sudden() -> Self {
+		Effect(EffectInner::Sudden)
+	}
 }
 
 // Implementation for Effect will print out either
@@ -104,25 +106,25 @@ pub struct Value(u32, ValueKind);
 //#[strum_discriminants(vis(pub))]
 //#[strum_discriminants(name(ValueKind))]
 pub enum ValueKind {
-        /// Color temperature of the lamp in kelvins. Must be within 1700-6500 K (inclusive).
+	/// Color temperature of the lamp in kelvins. Must be within 1700-6500 K (inclusive).
 	ColorTemp,
-        /// RGB value of the lamp. Must be within 0x0 ..= 0xFFFFFF.
-        ///
-        /// Using big-endian notation, the first byte needs to be zero. The second byte conveys the red value, ranging from 0x00 (eq. decimal 0) to 0xFF (eq. decimal 255). The third and fourth bytes convey the green and blue values respectively.
-        ///
-        /// That is, the RGB value as a u32 would be of the form 0x00rrggbb, where r,g,b are the
-        /// red,green,blue values respectively. As a little hint, the function u32::from_be_bytes()
-        /// can be of use when constructing the RGB value from individual red, green, and blue
-        /// components.
+	/// RGB value of the lamp. Must be within 0x0 ..= 0xFFFFFF.
+	///
+	/// Using big-endian notation, the first byte needs to be zero. The second byte conveys the red value, ranging from 0x00 (eq. decimal 0) to 0xFF (eq. decimal 255). The third and fourth bytes convey the green and blue values respectively.
+	///
+	/// That is, the RGB value as a u32 would be of the form 0x00rrggbb, where r,g,b are the
+	/// red,green,blue values respectively. As a little hint, the function u32::from_be_bytes()
+	/// can be of use when constructing the RGB value from individual red, green, and blue
+	/// components.
 	Rgb,
-        /// The hue in the HSV system. Must be within 0-359 (inclusive). Use together with
-        /// ValueKind::Sat.
+	/// The hue in the HSV system. Must be within 0-359 (inclusive). Use together with
+	/// ValueKind::Sat.
 	Hue,
-        /// The saturation in the HSV system. Must be within 0-100 (inclusive). Use together with
-        /// ValueKind::Hue.
+	/// The saturation in the HSV system. Must be within 0-100 (inclusive). Use together with
+	/// ValueKind::Hue.
 	Sat,
-        /// The brightness of the lamp in percent. Must be within 0-100 (inclusive). This can be
-        /// used independently of the other ValueKinds, as this controls brightness, not color.
+	/// The brightness of the lamp in percent. Must be within 0-100 (inclusive). This can be
+	/// used independently of the other ValueKinds, as this controls brightness, not color.
 	Bright,
 }
 
@@ -136,7 +138,7 @@ impl Value {
 	/// ```
 	/// Value::limit(ValueKind::Bright)
 	/// ```
-        /// will indicate that a valid brightness
+	/// will indicate that a valid brightness
 	/// value must be between 0 and 100 (incl. ends).
 	pub fn limit(kind: ValueKind) -> std::ops::RangeInclusive<u32> {
 		match kind {
@@ -170,7 +172,7 @@ impl Value {
 			})
 			*/
 		} else {
-			Err(String::from("Value out of bounds; should be in {lim}"))
+			Err(format!("Value out of bounds; should be in {lim:#?}"))
 		}
 	}
 
@@ -192,7 +194,7 @@ pub struct Command {
 	/// Type of the command.
 	kind: CommandKind,
 	/// The main parameter of the command. Ignored when using CommandKind::Toggle.
-	param_1: Value,
+	param_1: Option<Value>,
 	/// Optional second parameter. Ignored for other commands except CommandKind::SetHsv.
 	param_2: Option<Value>,
 	/// Transition effect and its speed, if smooth.
@@ -205,31 +207,69 @@ pub struct Command {
 #[derive(Clone, Copy, Debug, PartialEq, Eq, strum_macros::Display)]
 #[strum(serialize_all = "snake_case")]
 pub enum CommandKind {
-        /// Set the color temperature of the lamp.
+	/// Set the color temperature of the lamp.
 	SetCtAbx,
-        /// Set the lamp to some color described with RGB.
+	/// Set the lamp to some color described with RGB.
 	SetRgb,
-        /// Set the lamp to some color described with the HSV system.
-        ///
-        /// When using CommandKind::SetHsv, populate the param_1 and param_2 fields of Command with
-        /// a Value with ValueKind::Hue, and a Some(Value) with ValueKind::Sat. In the absence of a
-        /// value for param_2, a saturation of 100 will be used by default.
+	/// Set the lamp to some color described with the HSV system.
+	///
+	/// When using CommandKind::SetHsv, populate the param_1 and param_2 fields of Command with
+	/// a Value with ValueKind::Hue, and a Some(Value) with ValueKind::Sat. In the absence of a
+	/// value for param_2, a saturation of 100 will be used by default.
 	SetHsv,
-        /// Set the brightness of the lamp.
-        ///
-        /// This will not overwrite the color currently present on the lamp - it adjusts the
-        /// brightness of the lamp, making it brighter or darker.
+	/// Set the brightness of the lamp.
+	///
+	/// This will not overwrite the color currently present on the lamp - it adjusts the
+	/// brightness of the lamp, making it brighter or darker.
 	SetBright,
 	//SetPower,
-        /// Toggle the lamp on and off.
-        ///
-        /// If the lamp is on, turns it off. If the lamp is off, turns it on.
+	/// Toggle the lamp on and off.
+	///
+	/// If the lamp is on, turns it off. If the lamp is off, turns it on.
 	Toggle,
 }
 
+impl CommandKind {
+	/// Get the ValueKinds associated with this CommandKind.
+	///
+	/// The first element of the array corresponds to param_1 in Command, and the same for the
+	/// second element. None means that there is no related parameter.
+	pub fn associated(&self) -> [Option<ValueKind>; 2] {
+		match self {
+			CommandKind::SetCtAbx => [Some(ValueKind::ColorTemp), None],
+			CommandKind::SetRgb => [Some(ValueKind::Rgb), None],
+			CommandKind::SetHsv => [Some(ValueKind::Hue), Some(ValueKind::Sat)],
+			CommandKind::SetBright => [Some(ValueKind::Bright), None],
+			CommandKind::Toggle => [None; 2],
+		}
+	}
+}
+
 impl Command {
+	/// Print the (expected) associated ValueKinds with this Command.
+	pub fn associated(&self) -> [Option<ValueKind>; 2] {
+		self.kind.associated()
+	}
+
+	/// Determine whether the command is consistent (i.e. the parameters within correspond with
+	/// the CommandKind::associated().
+	pub fn is_consistent(&self) -> bool {
+		let expected = self.associated(); // what we expect to have
+		let get_kind = |p: Option<Value>| p.map(|p1| p1.get_kind());
+		// what we actually have
+		let actual = [get_kind(self.param_1), get_kind(self.param_2)];
+		expected == actual
+	}
+
 	/// Convert a Command to a String, given an integer to use as an ID.
-	pub fn to_request(&self, id: u8) -> String {
+	pub fn to_request(&self, id: u8) -> Result<String, String> {
+		// Consistency check
+		// This should catch errors like having a ColorTemp in a Brightness command
+		// and missing one of the two components of SetHsv
+		if !self.is_consistent() {
+			return Err(format!("Command is inconsistent. {:?}", self));
+		}
+
 		// For reference:
 		// Effect has a custom Display impl
 		// CommandKind has a Display impl from strum
@@ -237,25 +277,42 @@ impl Command {
 		let param_part: String = match self.kind {
 			// Dyadic command
 			CommandKind::SetHsv => {
+				// Check for the presence of parameters
+				if self.param_1.is_none() {
+					let emsg = "Hue missing from a SetHsv command";
+					error!("{}", &emsg);
+					return Err(emsg.to_string());
+				}
 				if self.param_2.is_none() {
 					info!("Saturation missing; assuming 100");
 				}
 				format!(
 					r#"{},{},{}"#,
-					self.param_1.get(),
+					self.param_1.expect("Expected Option to be Some(_)").get(),
 					self.param_2.map_or(100, |p| p.get()),
 					self.effect,
 				)
 			},
-                        // Command with no arguments
+			// Command with no arguments
 			CommandKind::Toggle => String::new(),
 			// Handle monadic commands here
-			_ => format!(r#"{},{}"#, self.param_1.get(), self.effect),
+			_ => {
+				if self.param_1.is_none() {
+					let emsg = "Value missing from a monadic command";
+					error!("{}", &emsg);
+					return Err(emsg.to_string());
+				}
+				format!(
+					r#"{},{}"#,
+					self.param_1.expect("Expected Option to be Some(_)").get(),
+					self.effect
+				)
+			},
 		};
-		format!(
+		Ok(format!(
 			concat!(r#"{{"id":{},"method":"{}","params":[{}]}}"#, "\r\n"),
 			id, self.kind, param_part
-		)
+		))
 	}
 }
 
