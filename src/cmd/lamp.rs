ff50e3ca691b5fda33d96a4b83544a70eff2f9b7
Gentrit Zenuni <gentrit.zenuni@gmail.com>
2025-10-31 02:47:31 +0200

    add getters for trait

--- src/cmd/lamp.rs
+++ src/cmd/lamp.rs
@@ -1,13 +1,14 @@
 use crate::cmd::{
-	objects::{InnerCommand, NoData},
-	traits::{CommaPrint, Command},
+	objects::{Effect, InnerCommand, NoData},
+	traits::Command,
 };
 
+// Macro for implementing constructors
 macro_rules! impl_new {
 	($name:ident,$ty:ty) => {
 		impl $name {
-			fn new(id: u8, params: $ty) -> Option<Self> {
-				let mby_me = Self(InnerCommand { id, params });
+			fn new(id: u8, params: $ty, effect: Effect) -> Option<Self> {
+				let mby_me = Self(InnerCommand { id, params, effect });
 				if !mby_me.is_valid() {
 					return None;
 				}
@@ -17,44 +18,45 @@ macro_rules! impl_new {
 	};
 }
 
+// Macro for implementing the getter methods of traits::Command
+macro_rules! impl_getters {
+	() => {
+		fn get_method(&self) -> String {
+			stringcase::snake_case(stringify!($name))
+			//stringify!($name).to_snake_case()
+		}
+
+		fn get_inner_request(&self) -> String {
+			self.0.inner_request()
+		}
+	};
+}
+
 pub struct SetCtAbx(InnerCommand<u16>);
+impl_new!(SetCtAbx, u16);
 pub struct SetHsv(InnerCommand<(u16, u8)>);
 impl_new!(SetHsv, (u16, u8));
 pub struct Toggle(InnerCommand<NoData>);
+impl_new!(Toggle, NoData);
 
 impl Command for SetCtAbx {
-	//type Params = u16;
-	/*
-	fn limit_cond() -> impl Fn(&Self::Params) -> bool {
-		|ct| (1700..=6500).contains(ct)
-	}
-	*/
-
+	impl_getters!();
 	fn is_valid(&self) -> bool {
 		(1700..=6500).contains(&self.0.params)
-	}
-
-	fn request(&self) -> String {
-		let foobar = format!("hello {}", self.0.params.comma_print());
-		let another_foobar = self.0.request();
-		todo!()
 	}
 }
 
 impl Command for SetHsv {
-	//type Params = (u16, u8);
-	/*
-	fn limit_cond() -> impl Fn(&Self::Params) -> bool {
-		|(hue, sat)| (0..=359).contains(hue) && (0..=100).contains(sat)
-	}
-	*/
-
+	impl_getters!();
 	fn is_valid(&self) -> bool {
 		(0..360).contains(&self.0.params.0) && (0..=100).contains(&self.0.params.0)
 	}
+}
 
-	fn request(&self) -> String {
-		todo!()
+impl Command for Toggle {
+	impl_getters!();
+	fn is_valid(&self) -> bool {
+		true
 	}
 }
 
